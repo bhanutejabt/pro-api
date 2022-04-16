6013b3a76a7c8a1e97391d7f0e75c6c973560267
bhanutejabt <bhanutejabt@gmail.com>
2022-04-17 00:11:10 +0530

    Added Courses Api

---
+++ src/main/java/io/spring/springbootstarter/course/CourseRepository.java
@@ -0,0 +1,15 @@
+package io.spring.springbootstarter.course;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+	
+	public List<Course> findByTopicId(String topicId);
+	
+	public List<Course> findByName(String name);
+	
+	public List<Course> findByDescription(String description);
+	
+}
